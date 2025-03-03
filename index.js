f03204a7c855c985f7f616267548d905be520654
Emman Oduor <wyllademanu003@gmail.com>
2025-03-03 06:12:41 +0300

    complete labs

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+// Create a new <h1> element and assign it to a variable `newHeader`
+const newHeader = document.createElement("h1");
+
+// Set the id of the new header
+newHeader.id = "victory";
+
+// Set the text content to "YOUR-NAME is the champion" (replace YOUR-NAME with your name)
+newHeader.textContent = "YOUR-NAME is the champion";
+
+// Append the new header to the body
+document.body.append(newHeader);
